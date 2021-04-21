af57afe10c78e3424e7ca8a7f60510b01cfcfe48
jaehwan <dkdlrk5208@gmail.com>
2021-04-21 12:14:58 +0900

    make MyRouter.js

--- MyServer.js
+++ MyServer.js
@@ -1,18 +1,23 @@
 const http = require('http');
+const url = require('url')
 const hostname = "localhost";
 const port = 8000;
 const baseUrl = 'http://' + hostname +  ':' + port;
 
-function start() {
+function start(route) {
     function onRequset(req, res){
+        const sBody = 'Hello, world!@';
+        console.log('Request receive');
+        pathname = new url.URL(req.url).pathname;
+        route(pathname);
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("Hello,world!");
+        res.write(sBody);
         res.end();
     }
     
     server = http.createServer(onRequset);
     server.listen(port,hostname);
-    console.log('Server is running at' + baseUrl);
+    console.log('Server is running at' + baseUrl).path;
 }
 
 
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "make MyServer.js"
+git commit -m "make MyRouter.js"
 git push -u origin master
