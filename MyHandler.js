10fb153f80502a181ce03f2451c3227bc3847687
jaehwan <dkdlrk5208@gmail.com>
2021-05-12 10:24:33 +0900

    add htmlFile function and fistHtml.html page

--- MyHandler.js
+++ MyHandler.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
+
 function start(res){
     let body = '<head> <meta charset="UTF-8"/></head>'
     body += '<body><div> Hello, start!@ <br> I am in the cloud class. </div><br>';
     body += '<div> <a href="/hello"> hello 페이지 </a> </div>';
     body += '<div> <a href="/wait"> 5초 대기 페이지 </a> </div>';
     body += '<div> <a href="/randomWait"> 무작위 대기 페이지 </a> </div>';
+    body += '<div> <a href="/firstHtml"> 파일 read 첫번째 페이지 </a> </div>';
     body += '</body>'
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(body);
@@ -36,7 +39,20 @@ function randomWait(res){
     }, waitTime);
 }
 
+function htmlFile(res, file){
+    body = fs.readFileSync(file, 'utf-8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(body);
+    res.end();
+}
+
+function firstHtml(res){
+    htmlFile(res, './firstHtml.html');
+
+}
+
 exports.start = start;
 exports.hello = hello;
 exports.wait = wait;
 exports.randomWait = randomWait;
+exports.firstHtml = firstHtml;
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "MyHandler href error"
+git commit -m "add htmlFile function and fistHtml.html page"
 git push -u origin master
